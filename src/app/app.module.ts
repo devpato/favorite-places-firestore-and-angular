63744dafe768a3a76ad96a0fba86cbd07949dd79
Pato Vargas <pevargas2730@gmail.com>
2019-11-25 00:40:59 -0500

    places list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlacesListComponent } from './places-list/places-list.component';
+import { PlacesFormComponent } from './places-form/places-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlacesListComponent,
+    PlacesFormComponent
   ],
   imports: [
     BrowserModule
