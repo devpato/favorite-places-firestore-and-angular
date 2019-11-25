d414c0f2a8d5fa1e51b2295428ea7ce0697320af
Pato Vargas <pevargas2730@gmail.com>
2019-11-25 11:31:41 -0500

    added table; from semantic ui

--- src/app/places-form/places-form.component.ts
+++ src/app/places-form/places-form.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-places-form',
-  templateUrl: './places-form.component.html',
-  styleUrls: ['./places-form.component.scss']
+  selector: "app-places-form",
+  templateUrl: "./places-form.component.html",
+  styleUrls: ["./places-form.component.scss"]
 })
 export class PlacesFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
