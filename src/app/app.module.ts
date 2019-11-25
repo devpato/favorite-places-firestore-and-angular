d5d60d913f773c5d8b3d6e9bb3ae2bcca758da58
Pato Vargas <pevargas2730@gmail.com>
2019-11-25 00:44:20 -0500

    places map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlacesListComponent } from './places-list/places-list.component';
 import { PlacesFormComponent } from './places-form/places-form.component';
+import { PlacesMapComponent } from './places-map/places-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlacesListComponent,
-    PlacesFormComponent
+    PlacesFormComponent,
+    PlacesMapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/places-map/places-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-places-map',
+  templateUrl: './places-map.component.html',
+  styleUrls: ['./places-map.component.scss']
+})
+export class PlacesMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
