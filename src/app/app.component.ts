bb05fa713d0ec195d6e4dbbe92e652d0587d19c6
Pato Vargas <pevargas2730@gmail.com>
2019-12-09 02:57:43 -0500

    added observable of places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlacesService } from './places.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'afire';
+
+  constructor(private placesService: PlacesService) {}
+  places$ = this.placesService.places$;
 }
