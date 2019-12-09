27582dba48efa3958808ed5f88c10cf95bd32ab7
Pato Vargas <pevargas2730@gmail.com>
2019-12-09 02:32:01 -0500

    added modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { AppComponent } from "./app.component";
 import { PlacesListComponent } from "./places-list/places-list.component";
 import { PlacesFormComponent } from "./places-form/places-form.component";
 import { PlacesMapComponent } from "./places-map/places-map.component";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,12 @@ import { PlacesMapComponent } from "./places-map/places-map.component";
     PlacesFormComponent,
     PlacesMapComponent
   ],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
