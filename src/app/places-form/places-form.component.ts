3ebe062524c7eefafeed02f5a1757a0ff293940a
Pato Vargas <pevargas2730@gmail.com>
2019-12-10 01:33:38 -0500

    form clean up

--- src/app/places-form/places-form.component.ts
+++ src/app/places-form/places-form.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
-import { PlacesService } from "../places.service";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { PlacesService } from '../places.service';
 
 @Component({
-  selector: "app-places-form",
-  templateUrl: "./places-form.component.html",
-  styleUrls: ["./places-form.component.scss"]
+  selector: 'app-places-form',
+  templateUrl: './places-form.component.html',
+  styleUrls: ['./places-form.component.scss']
 })
 export class PlacesFormComponent implements OnInit {
   form = new FormGroup({
-    name: new FormControl(""),
-    long: new FormControl(""),
-    lat: new FormControl("")
+    name: new FormControl(''),
+    long: new FormControl(''),
+    lat: new FormControl('')
   });
   constructor(private placesService: PlacesService) {}
 
