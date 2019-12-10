feae5aef457e0cf1f83d6919257f05629a556622
Pato Vargas <pevargas2730@gmail.com>
2019-12-10 01:35:28 -0500

    centering map

--- src/app/places-map/places-map.component.ts
+++ src/app/places-map/places-map.component.ts
@@ -8,8 +8,8 @@ import { Place } from '../place.model';
 })
 export class PlacesMapComponent implements OnInit {
   @Input() places: Place[];
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 53.2734;
+  lng = -7.77832031;
 
   constructor() {}
 
