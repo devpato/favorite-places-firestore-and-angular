0ce56a4ed4edc497090704b1913485ea3ba3aeee
Pato Vargas <pevargas2730@gmail.com>
2019-12-11 01:12:59 -0500

    added comment

--- src/app/places-map/places-map.component.ts
+++ src/app/places-map/places-map.component.ts
@@ -8,6 +8,8 @@ import { Place } from '../place.model';
 })
 export class PlacesMapComponent implements OnInit {
   @Input() places: Place[];
+
+  //This will define the center of the map
   lat = 53.2734;
   long = -7.77832031;
 
