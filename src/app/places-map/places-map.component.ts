ded56f9c320ed88966541c78f87c9432f3b1475f
Pato Vargas <pevargas2730@gmail.com>
2019-12-18 02:31:12 -0600

    removed lat and log for center

--- src/app/places-map/places-map.component.ts
+++ src/app/places-map/places-map.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Place } from '../place.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { Place } from "../place.model";
 
 @Component({
-  selector: 'app-places-map',
-  templateUrl: './places-map.component.html',
-  styleUrls: ['./places-map.component.scss']
+  selector: "app-places-map",
+  templateUrl: "./places-map.component.html",
+  styleUrls: ["./places-map.component.scss"]
 })
 export class PlacesMapComponent implements OnInit {
   @Input() places: Place[];
 
   //This will define the center of the map
-  lat = 53.2734;
-  long = -7.77832031;
 
   constructor() {}
 
