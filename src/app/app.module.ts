e87ae8420b078a9ef3d636adbf2000b3cca5847a
Pato Vargas <pevargas2730@gmail.com>
2019-12-18 02:29:01 -0600

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AgmCoreModule } from '@agm/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirePerformanceModule } from "@angular/fire/performance";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AgmCoreModule } from "@agm/core";
 
-import { AppComponent } from './app.component';
-import { PlacesListComponent } from './places-list/places-list.component';
-import { PlacesFormComponent } from './places-form/places-form.component';
-import { PlacesMapComponent } from './places-map/places-map.component';
-import { environment } from 'src/environments/environment';
+import { AppComponent } from "./app.component";
+import { PlacesListComponent } from "./places-list/places-list.component";
+import { PlacesFormComponent } from "./places-form/places-form.component";
+import { PlacesMapComponent } from "./places-map/places-map.component";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -23,8 +24,9 @@ import { environment } from 'src/environments/environment';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    AngularFirePerformanceModule,
     AgmCoreModule.forRoot({
-      apiKey: 'XXXXXXX'
+      apiKey: "XXXXXXXXXX"
     })
   ],
   providers: [],
