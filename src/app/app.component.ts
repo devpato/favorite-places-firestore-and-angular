072765b3ade0b323e5df46a510184b5e3a1c6729
Pato Vargas <pevargas2730@gmail.com>
2019-12-18 02:24:26 -0600

    added queryTotal trace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { PlacesService } from './places.service';
+import { Component, OnInit } from "@angular/core";
+import { PlacesService } from "./places.service";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFirePerformance } from "@angular/fire/performance";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'This Dot app';
+  title = "This Dot app";
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private pref: AngularFirePerformance,
+    private db: AngularFirestore
+  ) {}
   places$ = this.placesService.places$;
+
+  async loadPlacesData() {
+    const perf = await this.pref.performance.toPromise();
+    const trace = perf.trace("queryTotalTrace");
+    const places = await this.db
+      .collection("places")
+      .get()
+      .toPromise();
+    trace.incrementMetric("totalPlaces", places.size);
+    trace.stop();
+  }
 }
